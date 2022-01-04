bb64a33b3e64db5a8ae4f5ad96ae73b448ca46eb
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-01-04 16:56:06 +0800

    -Added images and index file as readme -Added help in module fields

--- models/product_template_inherited.py
+++ models/product_template_inherited.py
@@ -5,5 +5,5 @@ from odoo import api, fields, models
 class ProductTemplateInherited(models.Model):
     _inherit = "product.template"
 
-    x_show_in_ecommerce = fields.Boolean(store=True)
-    x_description_sale_ecommerce = fields.Text(store=True)
+    x_show_in_ecommerce = fields.Boolean(field_description="Show In Ecommerce", store=True, help="If this checkbox filled, this product will show in Central E-Commerce", ttype="boolean")
+    x_description_sale_ecommerce = fields.Text(field_description="Description Sale Ecommerce", store=True, help="A description of the Product that you want to communicate to your customers. This description will be pulled to E-Commerce", ttype="text")
