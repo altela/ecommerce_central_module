ba4484b92046b4aecb5858e24d7d6f08e097b5c9
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-01-07 10:18:30 +0800

    Change field_description with string to rename form

--- models/product_template_inherited.py
+++ models/product_template_inherited.py
@@ -5,6 +5,5 @@ from odoo import api, fields, models
 class ProductTemplateInherited(models.Model):
     _inherit = "product.template"
 
-    x_ecom_desc = fields.Char(default="E-Commerce")
-    x_show_in_ecommerce = fields.Boolean(field_description="Show In Ecommerce", store=True, help="If this checkbox filled, this product will show in Central E-Commerce", ttype="boolean")
-    x_description_sale_ecommerce = fields.Text(field_description="Description Sale Ecommerce", store=True, help="A description of the Product that you want to communicate to your customers. This description will be pulled to E-Commerce", ttype="text")
+    x_show_in_ecommerce = fields.Boolean(string="Show In Ecommerce", store=True, help="If this checkbox filled, this product will show in Central E-Commerce", ttype="boolean")
+    x_description_sale_ecommerce = fields.Text(string="Description Sale Ecommerce", store=True, help="A description of the Product that you want to communicate to your customers. This description will be pulled to E-Commerce", ttype="text")
